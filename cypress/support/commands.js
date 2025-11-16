0f7886323d77f316feb795b054e83a66b79dc9b1
Mariana Silva <71182554+Marianaap@users.noreply.github.com>
2025-11-16 13:56:07 -0300

    adiciona comandos auxiliares

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,42 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/// <reference types="cypress" />
+
+Cypress.Commands.add('login', (email, password) => {
+   return cy.request({
+      method: "POST",
+      url: "/login",
+      body: {
+         email,
+         password,
+      }
+   }).then((res) => {
+      expect(res.status).to.eq(200)
+      expect(res.body).to.have.property("message", "Login realizado com sucesso")
+      expect(res.body).to.have.property("authorization")
+
+      const token = res.body.authorization
+      cy.window().then((win) => {
+         win.localStorage.setItem("token", token)
+      })
+
+      return cy.wrap(token);
+   })
+})
+
+Cypress.Commands.add('loginSession', (email, password) => {
+   cy.session([email, password], () => {
+      cy.login(email, password)
+   })
+})
+
+Cypress.Commands.add("authRequest", (options) => {
+   cy.window().then((win) => {
+      const token = win.localStorage.getItem("token")
+      cy.request({
+         ...options,
+         headers: {
+            Authorization: token,
+            ...(options.headers || {}),
+         },
+      });
+   });
+})
